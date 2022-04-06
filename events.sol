e6fe32bbd5dec6d430f4901e16ad51d5528c9d8d
Arundhati <arundhati.gunu@gmail.com>
2022-04-06 08:55:02 +0530

    logging an event and emitting it

---
+++ events.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+contract Event {
+    // Event declaration
+    // Up to 3 parameters can be indexed.
+    // Indexed parameters helps you filter the logs by the indexed parameter
+    event Log(address indexed sender, string message);
+
+    function test() public {
+        emit Log(msg.sender, "Hello World!");
+        emit Log(msg.sender, "Hello EVM!");
+    }
+}
